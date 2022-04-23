26746888167186609d1e781e16d9cfadfc9b493e
Arna13 <51060310+Arna13@users.noreply.github.com>
2022-04-23 02:38:58 +0200

    minor update

--- futurerestore-inferius-linux.sh
+++ futurerestore-inferius-linux.sh
@@ -14,8 +14,8 @@ export PKG_CFG="/tmp/out/lib/pkgconfig"
 export CC_ARGS="CC=/usr/bin/clang-13 CXX=/usr/bin/clang++-13 LD=/usr/bin/ld.lld-13 RANLIB=/usr/bin/ranlib AR=/usr/bin/ar"
 export CONF_ARGS="--prefix=/tmp/out --disable-dependency-tracking --disable-silent-rules --disable-debug --without-cython --disable-shared"
 export CMAKE_ARGS="-DCMAKE_BUILD_TYPE=Release -DCMAKE_CROSSCOMPILING=true -DCMAKE_C_FLAGS=${C_ARGS} -DCMAKE_CXX_FLAGS=${CXX_ARGS} -DCMAKE_SHARED_LINKER_FLAGS=${LD_ARGS} -DCMAKE_STATIC_LINKER_FLAGS=${LD_ARGS} -DCMAKE_INSTALL_PREFIX=/tmp/out -DBUILD_SHARED_LIBS=0 -Wno-dev"
-export JNUM="-j1"
-export LNUM="-l1"
+export JNUM="-j$(nproc)"
+export LNUM="-l$(nproc)"
 export script_timer_start=$(date +%s)
 export DIR=$(pwd)
 
@@ -33,7 +33,7 @@ function getAPTDeps () {
     sed -i 's/deb\.debian\.org/ftp.de.debian.org/g' /etc/apt/sources.list
     apt-get -qq update
     apt-get -yqq dist-upgrade
-    apt-get install --no-install-recommends -yqq curl gnupg2 lsb-release wget software-properties-common build-essential git autoconf automake libtool-bin pkg-config cmake zlib1g-dev libminizip-dev libpng-dev libreadline-dev libbz2-dev libudev-dev libudev1
+    apt-get install --no-install-recommends -yqq curl gnupg2 lsb-release wget software-properties-common build-essential git autoconf automake libtool-bin pkg-config cmake zlib1g-dev libminizip-dev libpng-dev libreadline-dev libbz2-dev libudev-dev libudev1 python3-dev
     cp -LRP /usr/bin/ld ~/
     rm -rf /usr/bin/ld /usr/lib/x86_64-linux-gnu/lib{usb-1.0,png*}.so*
     curl -sO https://apt.llvm.org/llvm.sh
